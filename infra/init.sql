b88c3e4f5743a0b9fd4471443ee3dde7bedcc561
Lada Goglacheva <gog.lada.se@gmail.com>
2025-06-10 16:57:13 +0300

    починила еще несколько ошибок с рецептами

--- infra/init.sql
+++ infra/init.sql
@@ -1,7 +1,1 @@
-GRANT ALL PRIVILEGES ON DATABASE foodgram TO foodgram_user; 
-
--- Изменение сортировки для поля name в таблице ингредиентов
-ALTER TABLE recipes_ingredient ALTER COLUMN name TYPE VARCHAR(200) COLLATE "C";
-
--- Изменение сортировки для поля name в таблице рецептов
-ALTER TABLE recipes_recipe ALTER COLUMN name TYPE VARCHAR(200) COLLATE "C"; 
+GRANT ALL PRIVILEGES ON DATABASE foodgram TO foodgram_user;
